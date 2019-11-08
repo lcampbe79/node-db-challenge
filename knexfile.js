c7b2a5d584eb9d5a9d9ba67fcf54de255637db97
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-11-08 08:33:13 -0800

    Added migrations.

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,12 @@ module.exports = {
     connection: {
       filename: './data/projects.db3'
     },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
     useNullAsDefault: true,
   },
   pool: {
